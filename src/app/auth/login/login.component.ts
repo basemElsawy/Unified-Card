590b8ae955e6faaa0e3a9719315abec4fc67ff59
Basem mohamed abuzaid <besoelpop2@gmail.com>
2024-01-29 15:37:56 +0200

    sign up and login pages finished html and css template

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  lang: string = 'en';
+  loginForm!: FormGroup<any>;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+  submitForm() {
+    console.log(this.loginForm.value);
+  }
+  navigate() {
+    this.router.navigate(['/signup']);
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, ReactiveFormsModule, RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  lang: string = 'en';
+  loginForm!: FormGroup<any>;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      mobileNumber: new FormControl('', Validators.required),
+      emailAddress: new FormControl('', Validators.required),
+      identificationNumber: new FormControl('', Validators.required),
+      fullName: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
+    });
+  }
+  submitForm() {
+    console.log(this.loginForm.value);
+  }
+  navigate() {
+    this.router.navigate(['/login']);
+  }
+}
