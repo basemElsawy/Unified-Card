8c9493cf4e4b2a110a4ff2c5d2d96a7160eb00b0
Ali8970 <aliabdelftah104@gmail.com>
2024-01-31 11:15:25 +0200

    create home page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { RetrievePasswordComponent } from './auth/login/retrieve-password/retrieve-password.component';
+<<<<<<< HEAD
 import { LocalDelightsComponent } from './Home-components/local-delights/local-delights.component';
 import { TransportationMapComponent } from './Home-components/transportation-map/tansportation-map.component';
+=======
+import { HomeComponent } from './components/home/home.component';
+>>>>>>> 0da594a (create home page component)
 export const routes: Routes = [
   {
     path: 'login',
@@ -19,7 +23,14 @@ export const routes: Routes = [
     path: 'signup',
     component: SignupComponent,
   },
+<<<<<<< HEAD
 
   { path: 'local-delights', component: LocalDelightsComponent },
   { path: 'tansportation-map', component: TransportationMapComponent },
+=======
+  {
+    path: '',
+    component: HomeComponent,
+  },
+>>>>>>> 0da594a (create home page component)
 ];
