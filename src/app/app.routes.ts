53d31675f73cd08be3a21b0524d67cdb6ae6f82c
Basem mohamed abuzaid <besoelpop2@gmail.com>
2024-01-31 15:44:44 +0200

    profile design half way in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { RetrievePasswordComponent } from './auth/login/retrieve-password/retrieve-password.component';
 import { PersonalProfileComponent } from './components/profile/personal-profile.component';
+import { MyCardComponent } from './components/profile/my-card/my-card.component';
+import { HistoryComponent } from './components/profile/history/history.component';
+import { BalanceComponent } from './components/profile/balance/balance.component';
+import { MyTripsComponent } from './components/profile/my-trips/my-trips.component';
 export const routes: Routes = [
   {
     path: 'login',
--- src/app/components/profile/personal-profile.component.ts
+++ src/app/components/profile/personal-profile.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+import { MyTripsComponent } from './my-trips/my-trips.component';
+import { MyCardComponent } from './my-card/my-card.component';
+import { BalanceComponent } from './balance/balance.component';
+import { HistoryComponent } from './history/history.component';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-personal-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    RouterLinkActive,
+    MyTripsComponent,
+    MyCardComponent,
+    BalanceComponent,
+    HistoryComponent,
+  ],
   templateUrl: './personal-profile.component.html',
-  styleUrl: './personal-profile.component.css'
+  styleUrl: './personal-profile.component.css',
 })
 export class PersonalProfileComponent {
+  active!: string;
+  activeSubject: BehaviorSubject<string> = new BehaviorSubject('My Trips');
+  buttons: { id: number; link: string; isActive: boolean }[] = [
+    {
+      id: 1,
+      link: 'My Trips',
+      isActive: true,
+    },
+    {
+      id: 2,
+      link: 'My Card',
+      isActive: false,
+    },
+    {
+      id: 3,
+      link: 'Balance',
+      isActive: false,
+    },
+    {
+      id: 4,
+      link: 'History',
+      isActive: false,
+    },
+  ];
 
+  ngOnInit() {
+    this.activeSubject.subscribe((link) => {
+      this.active = link;
+    });
+  }
+  changeStateButton(
+    selectedButton: { id: number; link: string; isActive: boolean },
+    state: string
+  ) {
+    this.activeSubject.next(state);
+    selectedButton.isActive = true;
+    for (let button of this.buttons) {
+      if (button.id != selectedButton.id) {
+        button.isActive = false;
+      }
+    }
+  }
 }
