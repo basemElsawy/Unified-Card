a35d4321c0b623e4a29c8a71bfb1a0787d779812
nuha <nohanuha8@gmail.com>
2024-02-07 15:38:48 +0200

    transportaion map 70% done

--- src/app/Home-components/transportation-map/tansportation-map.component.ts
+++ src/app/Home-components/transportation-map/tansportation-map.component.ts
@@ -6,16 +6,27 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-tansportation-map',
   templateUrl: './tansportation-map.component.html',
   styleUrls: ['./tansportation-map.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, InputTextModule],
 })
 export class TransportationMapComponent implements OnInit {
+  planForm!: FormGroup;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initPlanForm();
+  }
+
+  initPlanForm() {
+    this.planForm = new FormGroup({
+      currentLocation: new FormControl(null),
+      destination: new FormControl(null),
+    });
+  }
 }
