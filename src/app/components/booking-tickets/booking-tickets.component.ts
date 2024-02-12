2606b2fedb39f8425b3529b88b8a1fd500dda520
Ali8970 <aliabdelftah104@gmail.com>
2024-02-12 15:33:11 +0200

    create component

---
+++ src/app/components/booking-tickets/booking-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-tickets.component.html',
+  styleUrl: './booking-tickets.component.css'
+})
+export class BookingTicketsComponent {
+
+}
