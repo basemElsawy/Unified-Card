b5653291769dc49e0c98fb15016264040f655462
Ali8970 <aliabdelftah104@gmail.com>
2024-03-04 11:04:41 +0200

    last

--- src/app/components/home-banner/home-banner.component.ts
+++ src/app/components/home-banner/home-banner.component.ts
@@ -24,46 +24,46 @@ export class HomeBannerComponent {
       route: string;
     };
   }[] = [
-    {
-      image: '../../../assets/pic2.png',
-      mainHeadline:
-        '<h1>All Around Egypt in your <span style="color:#9CE0B2;">pocket</span> </h1>',
-      subHeadline: 'new unified card for every egyptian citizen',
-      isActive: true,
-    },
-    {
-      image: '../../../assets/pic1.png',
-      mainHeadline:
-        '<h1>Reserve All Kinds Of <span style="color:#9CE0B2;">Subway</span> Tickets</h1>',
-      subHeadline: 'or use our integrated maps as a guide to your destination',
-      isActive: false,
-      button1: { title: 'Metro Guide', route: '' },
-      button2: { title: 'Metro Tickets', route: '' },
-    },
-    {
-      image: '../../../assets/pic3.png',
-      mainHeadline:
-        '<h1>Find Your Way Through <span style="color:#9CE0B2;">Egypt</span></h1>',
-      subHeadline: 'provide multiple plans for your destination',
-      isActive: false,
-      button1: { title: 'Guide Map', route: '' },
-    },
-    {
-      image: '../../../assets/pic4.png',
-      mainHeadline: '<h1>Find a suitable bus for your trip</h1>',
-      subHeadline: 'provide multiple plans for your destination',
-      button1: { title: 'Bus Guide', route: '' },
-      isActive: false,
-      button2: { title: 'Bus Tickets', route: '' },
-    },
-  ];
+      {
+        image: '../../../assets/pic2.png',
+        mainHeadline:
+          '<h1>All Around Egypt in your <span style="color:#9CE0B2;">pocket</span> </h1>',
+        subHeadline: 'new unified card for every egyptian citizen',
+        isActive: true,
+      },
+      {
+        image: '../../../assets/pic1.png',
+        mainHeadline:
+          '<h1>Reserve All Kinds Of <span style="color:#9CE0B2;">Subway</span> Tickets</h1>',
+        subHeadline: 'or use our integrated maps as a guide to your destination',
+        isActive: false,
+        button1: { title: 'Metro Guide', route: '' },
+        button2: { title: 'Metro Tickets', route: '' },
+      },
+      {
+        image: '../../../assets/pic3.png',
+        mainHeadline:
+          '<h1>Find Your Way Through <span style="color:#9CE0B2;">Egypt</span></h1>',
+        subHeadline: 'provide multiple plans for your destination',
+        isActive: false,
+        button1: { title: 'Guide Map', route: '' },
+      },
+      {
+        image: '../../../assets/pic4.png',
+        mainHeadline: '<h1>Find a suitable bus for your trip</h1>',
+        subHeadline: 'provide multiple plans for your destination',
+        button1: { title: 'Bus Guide', route: '' },
+        isActive: false,
+        button2: { title: 'Bus Tickets', route: '' },
+      },
+    ];
   responsiveOptions!: {
     breakpoint: string;
     numVisible: number;
     numScroll: number;
   }[];
 
-  constructor(public sanitizer: DomSanitizer) {}
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     // setInterval(() => {
