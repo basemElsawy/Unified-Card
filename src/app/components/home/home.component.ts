fbddeb9c1fe74927d06b0df00ad8a166897fa906
Ali8970 <aliabdelftah104@gmail.com>
2024-03-12 11:52:44 +0200

    add footer section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { HomeBannerComponent } from '../home-banner/home-banner.component';
 import { BookingTicketsComponent } from '../booking-tickets/booking-tickets.component';
 import { LocalDelightsComponent } from '../Home-components/local-delights/local-delights.component';
 import { TransportationMapComponent } from '../Home-components/transportation-map/tansportation-map.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -13,8 +14,9 @@ import { TransportationMapComponent } from '../Home-components/transportation-ma
     BookingTicketsComponent,
     LocalDelightsComponent,
     TransportationMapComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent { }
